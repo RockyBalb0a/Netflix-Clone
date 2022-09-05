2216407840a7cf670c261b734fcae2752e82d5a7
Mayank Shekhar <mayank.shekhar2019@iem.edu.in>
2022-09-05 21:40:48 +0530

    Third commit

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,5 @@
-const API_KEY = "df1b14e24ed3b413c11ee1d416db78d0";
-
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
